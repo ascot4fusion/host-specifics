04602cdd1cbae91f79dcf1e89b544623dfb2e562
Mathieu Peybernes <mpeybern@login05.pitagora.local>
2025-06-10 17:39:13 +0200

    update scripts for Pitagora gpu

--- cineca-pitagora/compile-gpu.sh
+++ cineca-pitagora/compile-gpu.sh
@@ -1,1 +1,7 @@
-#TBD
+#!/bin/bash
+module purge
+module load nvhpc/24.9 
+module load hpcx-mpi/2.20 
+module load hdf5/1.14.3--hpcx-mpi--2.20--nvhpc--24.9
+make clean
+make ascot5_main MPI=1 GPU=1 ACC=1 CC=mpicc FLAGS="-L$(HDF5_ROOT)/lib -I$(HDF5_ROOT)/include" -j
--- cineca-pitagora/submit-gpu.sh
+++ cineca-pitagora/submit-gpu.sh
@@ -1,1 +1,12 @@
-#TBD
+#!/bin/bash -l
+#SBATCH --job-name=test
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=16
+#SBATCH --gres=gpu:1
+#SBATCH --partition=gpu
+#SBATCH --time=01:00:00
+ulimit -s unlimited
+export OMP_NUM_THREADS=1
+srun -n 1 ./ascot5_main 
+
