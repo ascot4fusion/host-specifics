e624cb80eff1474cbc4eb9c4b712178b181bf437
Mathieu Peybernes <80824248+peyberne@users.noreply.github.com>
2026-03-13 14:07:47 +0100

    compile script on Pitagora cpu

--- cineca-pitagora/compile-cpu.sh
+++ cineca-pitagora/compile-cpu.sh
@@ -1,1 +1,6 @@
-#TBD
+#!/bin/bash
+module purge
+module load gcc/12.3.0 
+module load hdf5/1.14.3--openmpi--4.1.6--gcc--12.3.0 
+make clean
+make ascot5_main -j MPI=1 OMP=1 NSIMD=8 CC=mpicc  FLAGS=" --offload=disable"
