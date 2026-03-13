c4b5c1ecbed1cb4c1476edbac65db9bb7b4e535e
Mathieu Peybernes <80824248+peyberne@users.noreply.github.com>
2026-03-13 14:27:12 +0100

    SLURM script for CPU job submission on Pitagora
    Added SLURM job configuration and environment setup.

--- cineca-pitagora/submit-cpu.sh
+++ cineca-pitagora/submit-cpu.sh
@@ -1,1 +1,12 @@
-#TBD
+#!/bin/bash -l
+#SBATCH --job-name=test
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=2
+#SBATCH --cpus-per-task=128
+#SBATCH --partition=dcgp_fua_prod
+#SBATCH --time=01:00:00
+module load gcc/12.3.0 
+module load hdf5/1.14.3--openmpi--4.1.6--gcc--12.3.0
+ulimit -s unlimited
+export OMP_NUM_THREADS=128
+srun ascot5_main
